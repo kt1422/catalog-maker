887aecd2de86580228bf961333cde5bcddd201f7
Kenneth Tan <kennethtan1414@gmail.com>
2023-10-31 11:13:40 +0800

    start

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+if(isset($_GET['page'])){
+    
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Catalog Maker</title>
+</head>
+<body>
+    
+</body>
+</html>
